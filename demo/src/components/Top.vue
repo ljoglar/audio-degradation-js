a83e42be396dccf206436199e070b1c430794a1f
Luis Joglar Ongay <ljoglar@protonmail.com>
2022-10-16 21:34:59 +0200

    update header demo

--- demo/src/components/Top.vue
+++ demo/src/components/Top.vue
@@ -6,9 +6,11 @@
     <h1 class="green"> Audio Degradation Toolkit in JavaScript</h1>
     <h2>
       This website is a demo of the <a target="_blank" href="https://github.com/ljoglar/audio-degradation-js" class="link-secondary">JavaScript version of the Audio Degradation Toolkit</a>
-      used in real-time with Web Audio Worklets.
+      used the <a target="_blank" href="https://www.w3.org/TR/webaudio/" class="link-secondary">Web Audio API</a>,
+      <a target="_blank" href="https://vuejs.org/" class="link-secondary">Vue.js</a>,
+      and <a target="_blank" href="https://wavesurfer-js.org/" class="link-secondary">wavesurfer.js</a>.
     </h2>
-    <h3>Original <a target="_blank" href="https://code.soundsoftware.ac.uk/projects/audio-degradation-toolbox" class="link-secondary">Audio Degradation Toolbox</a></h3>
+    <h3>Original <a target="_blank" href="https://code.soundsoftware.ac.uk/projects/audio-degradation-toolbox" class="link-success">Audio Degradation Toolbox</a></h3>
   </div>
 </template>
 
